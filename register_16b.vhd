8ffc77d10c98cc5f28745b2402b913253ae36301
srbh001 <22B3905@iitb.ac.in>
2023-11-23 23:29:57 +0530

    Fix 16 Bit PIP register

--- register_16b.vhd
+++ register_16b.vhd
@@ -5,7 +5,7 @@ library work;
 use work.all;
 
 entity Register6bit is
-port(Clk, Reset : in std_logic;
+port(Clk, Reset, write : in std_logic;
 data_in : in std_logic_vector(15 downto 0);
 data_out : out std_logic_vector(15 downto 0));
 end entity;
@@ -18,12 +18,15 @@ architecture struct of Register6bit is
             Q,Qbar : buffer std_logic);
     end entity D_FF ;
 	signal buffer_qbar :std_logic_vector(15 downto 0);
+    signal data_out_buffer : std_logic_vector(15 downto 0);
+    signal sig_write;
     
 begin
 	
     gen : for i in 0 to 15 generate
-        dff : D_FF port map (D=> data_in(i), En => '1', reset => Reset, pre => '0', clk => Clk, Q=> data_out(i), Qbar => buffer_qbar(i));
+        sig_write <= (write and data_in(i)) or (not write and data_out_buffer(i));
+        dff : D_FF port map (D=> sig_write, En => '1', reset => Reset, pre => '0', clk => Clk, Q=> data_out_buffer(i), Qbar => buffer_qbar(i));
     end generate gen;
-
+    data_out <= data_out_buffer;
 
 end architecture struct;
