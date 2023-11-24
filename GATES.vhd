e74d6b4e6c0eac38c57c4cf9b10af4efe9c1365d
srbh001 <22B3905@iitb.ac.in>
2023-11-24 17:53:51 +0530

    fix : Reg_file and Gates compile without any issues

--- GATES.vhd
+++ GATES.vhd
@@ -8,9 +8,16 @@ package Gates is
   end component str_match;
 
   component TwoxOneMux is -- 2x1 Mux with 8 bit inputs
-    port (A, B, Sel: in  std_logic_vector(7 downto 0); 
+    port (A, B: in  std_logic_vector(7 downto 0);
+    Sel: std_logic; 
         Y: out std_logic);
   end component TwoxOneMux;
+
+  component EighttoOneMux is 
+    port (A, B, C, D, E, F, G, H: in std_logic;
+        Sel: in std_logic_vector(2 downto 0);
+          Y: out std_logic);
+end component EighttoOneMux;
 
 end package Gates;
 
@@ -35,15 +42,39 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity TwoxOneMux is
-  port (A, B, S: in  std_logic_vector(7 downto 0); 
+  port (A, B : in  std_logic_vector(7 downto 0); 
+        Sel: in std_logic;
         Y: out std_logic);
 end entity TwoxOneMux;
 
 architecture Equations of TwoxOneMux is
 begin
     gen: for i in 0 to 7 generate
-           Y <= (A(i) and not Sel) or (B(i) and Sel);
+           Y(i) <= (A(i) and not Sel) or (B(i) and Sel);
         end generate;
     end generate;
     
 end Equations;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity EighttoOneMux is 
+    port (A, B, C, D, E, F, G, H: in std_logic;
+        Sel: in std_logic_vector(2 downto 0);
+          Y: out std_logic);
+    end entity EighttoOneMux;
+
+architecture struct of EighttoOneMux is
+begin 
+    Y <= (A and (not sel(0) and not sel(1) and not sel(2) )) or 
+        (B and (not sel(0) and not sel(1) and sel(2) )) or
+        (C and (not sel(0) and sel(1) and not sel(2) )) or
+        (D and (not sel(0) and sel(1) and sel(2) )) or
+        (E and (sel(0) and not sel(1) and not sel(2) )) or
+        (F and (sel(0) and not sel(1) and sel(2) )) or
+        (G and (sel(0) and sel(1) and not sel(2) )) or
+        (H and (sel(0) and sel(1) and sel(2) ));
+end struct;
+
--- Reg_file.vhd
+++ Reg_file.vhd
@@ -2,7 +2,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 library work;
 use work.GATES.all;
-use ieee.numeric_std_unsigned.all;
+use ieee.numeric_std.all;
+
+
  -- Register File Works as follows :
     -- has 8 registers and each register is 16 bits 
     -- has 3 inputs : 
@@ -14,7 +16,7 @@ use ieee.numeric_std_unsigned.all;
 entity Reg_File is
     port(
         clk : in std_logic;
-        Write : in std_logic; -- Flag to write - should be 1 if writing
+        Writeff : in std_logic; -- Flag to write - should be 1 if writing
         readReg1 : in std_logic_vector(2 downto 0); -- A1
         readReg2 : in std_logic_vector(2 downto 0); -- A2
         writeReg : in std_logic_vector(2 downto 0); -- A3
@@ -27,47 +29,119 @@ end Reg_File;
 architecture struct of Reg_File is
 
     component Register_16b is 
-        port(Clk, Reset, write : in std_logic;
+        port(Clk, Reset, writef : in std_logic;
         data_in : in std_logic_vector(15 downto 0);
         data_out : out std_logic_vector(15 downto 0));
     end component;
 
     signal writeDataBuff1, writeDataBuff2 : std_logic_vector(15 downto 0) := (others => '0');
+    signal reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7 : std_logic_vector(15 downto 0) := (others => '0');
     signal writeRegBuff1, writeRegBuff2 : std_logic_vector(2 downto 0) := (others => '0');
     signal writeFlag : std_logic_vector(7 downto 0) := (others => '0'); -- 8 bit flag to indicate which register to write to
     signal readA, readB : std_logic_vector(15 downto 0) := (others => '0');
-    signal readf1, readf2 : std_logic_vector(7 downto 0) := (others=>'0');
+    --signal readf1, readf2 : std_logic_vector(7 downto 0) := (others=>'0');
 
     begin
 
  
     
     gen_reg : for i in 0 to 7 generate
-        i_match : str_match port map (a => writeReg, b => std_logic_vector(to_unsiged(i),3), y => writeFlag(i)); -- returns '1' if i matches writeReg
-        j_match : str_match port map (a => readReg1, b => std_logic_vector(to_unsiged(i),3), y => readf1(i)); -- returns '1' if i matches readReg1
-        k_match : str_match port map (a => readReg2, b => std_logic_vector(to_unsiged(i),3), y => readf2(i)); -- returns '1' if i matches readReg2
-        regW : Register_16b port map(
+        i_match : str_match port map (a => writeReg, b => std_logic_vector(to_unsigned(i,3)), S => writeFlag(i)); -- returns '1' if i matches writeReg
+    
+    
+    end generate;
+
+---- REGISTERS COMPONENTS ----
+
+    regW1 : Register_16b port map(
             Clk => clk,
             Reset => '0',
-            write => writeFlag(i) and Write ,
+            writef => writeFlag(0) and Writeff ,
             data_in => writeData,
-            data_out => writeDataBuff2
-        );
+            data_out => reg0
+    );
+    regW2 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(1) and Writeff ,
+            data_in => writeData,
+            data_out => reg1
+    );
+    regW3 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(2) and Writeff ,
+            data_in => writeData,
+            data_out => reg2
+    );
 
-        regA_mux : TwoxOnemux port map(
-            a => readA,
-            b => writeDataBuff2,
-            sel => readf1(i),
-            y => readA
-        );
-        regB_mux : TwoxOnemux port map(
-            a => readB,
-            b => writeDataBuff2,
-            sel => readf2(i),
-            y => readB
-        );
+    regW4 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(3) and Writeff ,
+            data_in => writeData,
+            data_out => reg3
+    );
+    regW5 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(4) and Writeff ,
+            data_in => writeData,
+            data_out => reg4
+    );
+    regW6 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(5) and Writeff ,
+            data_in => writeData,
+            data_out => reg5
+    );
+    regW7 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(6) and Writeff ,
+            data_in => writeData,
+            data_out => reg6
+    );
+    regW8 : Register_16b port map(
+            Clk => clk,
+            Reset => '0',
+            writef => writeFlag(7) and Writeff ,
+            data_in => writeData,
+            data_out => reg7
+    );
+    
+        
     
 
+    
+
+    gene_read : for i in 0 to 15 generate
+        eightOneM : EighttoOneMux port map (
+            a => reg0(i),
+            b => reg1(i),
+            c => reg2(i),
+            d => reg3(i),
+            e => reg4(i),
+            f => reg5(i),
+            g => reg6(i),
+            h => reg7(i),
+            sel => readReg1,
+            y => readA(i)
+        );
+        eightOneM2 : EighttoOneMux port map (
+            a => reg0(i),
+            b => reg1(i),
+            c => reg2(i),
+            d => reg3(i),
+            e => reg4(i),
+            f => reg5(i),
+            g => reg6(i),
+            h => reg7(i),
+            sel => readReg2,
+            y => readB(i)
+        );
+    
     end generate;
 
         readData1 <= readA;
