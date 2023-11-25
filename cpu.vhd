7dfaa2feae63bc2487f037a04e203bd269d31112
srbh001 <22B3905@iitb.ac.in>
2023-11-25 20:14:24 +0530

    add: CPU - other states

--- cpu.vhd
+++ cpu.vhd
@@ -51,6 +51,17 @@ architecture struct of CPU is
     data_out : out std_logic_vector(15 downto 0));
     end component Register_16b;
 
+   
+    component SE_6to16 is
+        port (A : in std_logic_vector(5 downto 0); Y : out std_logic_vector(15 downto 0));
+    end component SE_6to16 ;
+
+    component SE_9to16 is
+        port (A : in std_logic_vector(8 downto 0); Y : out std_logic_vector(15 downto 0));
+    end component SE_9to16 ;
+
+   
+
     --- REGISTER FILE SIGNALS ---
     signal RF_A1, RF_A2, RF_A3 : std_logic_vector(2 downto 0) := (Others =>'0');
     signal RF_D1, RF_D2, RF_D3: std_logic_vector(15 downto 0):= (Others =>'0');
@@ -77,6 +88,12 @@ architecture struct of CPU is
     signal IP : std_logic_vector(15 downto 0):= (Others =>'0');
     signal IR : std_logic_vector(15 downto 0):= (Others =>'0');
 
+     -- SIGN EXTENDERS --
+     signal SGN9_IN: std_logic_vector(8 downto 0);
+     signal SGN6_IN: std_logic_vector(5 downto 0);
+     signal SGN9_OUT, SGN6_OUT : std_logic_vector(15 downto 0);
+    
+
 
 begin
 
@@ -94,6 +111,9 @@ begin
     T2_INS : Register_16b port map (Clk => clk, Reset => reset, writef => T2_WRITE, data_in => T2_IN, data_out => T2_OUT);
     T3_INS : Register_16b port map (Clk => clk, Reset => reset, writef => T3_WRITE, data_in => T3_IN, data_out => T3_OUT);
 
+    -- SIGN EXTENDERS --
+    SGN9TO16 :  SE_9to16 port map(A=> SGN9_IN, Y=> SGN9_OUT);
+    SGN6TO16 : SE_6to16 port map (A=> SGN6_IN, Y=> SGN6_OUT);
 
     state_clk_proc: PROCESS(clk, reset, state)
     begin
@@ -108,7 +128,12 @@ begin
                     IR <= MEM_DATA_OUT;
 
                     A <= IP;
-                    B <= "0000000000000010";
+                    if ((IR(15 downto 11) = "1111") or (IR(15 downto 0) = "1101") ) then
+                        B <= "0000000000000000"
+                    else
+                        B <= "0000000000000010";
+                    end if;
+
                     PQR <= "000"
                     IP <= C;
 
@@ -160,23 +185,103 @@ begin
                     T3_IN <= C;
                     state <= S4;
                 
-                when S7=> -- NEEDS A DECODER
-                    A<= T1_OUT; 
-                    
-
-
-            
-
-
                 
+                when S7=>
+                    A <= T1_OUT;
+                    SGN6_IN <= IR(5 downto 0);
+                    B <= SGN6_OUT;
+                    PQR <= "000";
+                    T3_IN<= C;
 
+                    case IR(15 downto 12) is 
+                        when "1010" =>
+                            state <= S8;
+                        when "1011"=>
+                            state <= S13;
+                        when "0001"=>
+                            state <= S4_ADI;
+                        when others =>
+                            state => S1; -- FATAL: GOING BACK TO ONE IN CASE OF ERROR
+                    end case;
 
-                -- LAYER FOUR STATES -- 
+                when S3_LHI =>
+                        T3_IN <= IR(7 downto 0) & "00000000";
+                        state <= S4_LLI;
+                
+                when S3_LLI =>
+                    T3_IN <= "00000000" & IR(7 downto 0);
+                    state <= S4_LLI;
+                
+                when S16 =>
+                    A <= T1_OUT;
+                    B <= T2_OUT;
+                    PQR <= "001"; -- #TODO MAY NEED TO CHANGE THIS AS WELL
+                    -- ZERO_FLAG (Z) : IS IDEALLY WRITTEN AUTOMATICALY
+
+                when S20 =>
+                    -- TODO 
+                        state<= S1;
+                
+                when S3_JLR =>
+                    RF_A3 <= IR (11 downto 9);
+                    RF_D3 <= IP;
+                    IP <= T2_OUT;
+                    state <= 1;
+                
+        
+                
+            -- LAYER FOUR STATES -- 
+
                 when S4=> -- FINAL STATE FOR R TYPE INSTRUCTIONS
                     RF_D3 <= T3_OUT;
                     RF_A3 <= IR(5 downto 3);
-
                     state<= S1;
+                
+                when S4_LLI =>
+                    RF_D3 <= T3_OUT;
+                    RF_A3 <= IR (11 downto 9);
+                    
+                    state <= S1;
+                
+                when S17=>
+                        A <= IP;
+                        SGN6_IN <= IR(5 downto 0);
+                        B<= SGN6_OUT;
+                        if (zero_flag =1) then
+                                IP <= C;
+                        end if;
+                        
+                        state <= S1;
+
+                when S8 =>
+                    MEM_ADD <= T3_OUT;
+                    T3_IN <= MEM_OUT;
+                    state <= S9;
+                
+                when S13 =>
+                    MEM_ADD <= T3_OUT;
+                    MEM_DATA <= T2_OUT;
+                    state <= S1;
+                
+                
+                
+                ---- LAYER 5 ---
+
+                when S9 =>
+                    RF_D3 <= T3_OUT;
+                    RF_A3 <= IR( 8 downto 6);
+                    state <= S1;
+                
+                
+
+                
+                
+                
+
+
+                
+                -- TODO: ADD S4_ADI and RESOLVE FOR S20.
+
                 
                 
                         
